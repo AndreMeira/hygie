f4f52d94ddaf5a601339c49aa5bdce7513d0e5e4
Andremeira <andremeira@andremeira.com>
2022-12-08 15:20:36 +0100

    latest code

---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app-bar
+    color="#f46c7c"
+    dark
+    prominent
+    app
+    shrink-on-scroll
+    :src="require('@/assets/fond_vignette_hygie_one.jpg')">
+
+    <v-btn class="ma-2" fab dark small color="#f14156" @click="$router.push({ name: 'Home'})">
+      <v-icon dark>mdi-home</v-icon>
+    </v-btn>
+    <v-toolbar-title>
+      HYGIE//ONE
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+    <v-menu
+        left
+        bottom
+        v-if="$store.getters.user"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon dark>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>
+              <v-btn @click="logout" flat color="white">
+                page principale
+                <v-icon right>mdi-logout</v-icon>
+              </v-btn>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    <!--  -->
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+
+  methods:{
+    logout() {
+      this.$store.dispatch("logout user").then(() => {
+        this.$router.push({ name: 'Login'})
+        console.log("in logout success")
+      }).catch(e => {
+        this.$router.push({ name: 'Home'})
+        console.log("in logout failed")
+      })
+    }
+  }
+
+}
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,102 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <div class="divider" />
+    </v-row>
+    
+    <v-row justify="center" v-if="$store.getters.user">
+      <v-col cols="12" md="3" sm="12">
+          <v-card max-width="500" class="mx-auto">
+            <v-img
+              max-width="500"
+              :src="require('@/assets/body-params.jpg')"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>Tes données corporelles</v-card-title>
+            </v-img>
+
+            <v-card-actions>
+              <v-btn text color="#f14156" @click="$router.push({ name: 'Account'})">
+                Saisir tes données corporelles
+              </v-btn>
+              <v-spacer></v-spacer>
+              
+
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12" md="3" sm="12" v-if="this.currentUser.body">
+
+          <v-card max-width="500" class="mx-auto">
+            <v-img
+              max-width="500"
+              :src="require('@/assets/daily-data.jpg')"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>Apports quotidiens</v-card-title>
+            </v-img>
+
+            <v-card-actions>
+              <v-btn text color="#f14156" @click="$router.push({ name: 'DailyDataForm'})">
+                Saisie tes apports quotidiens
+              </v-btn>
+              <v-spacer></v-spacer>
+             
+            </v-card-actions>
+          </v-card>
+      </v-col>
+
+      <v-col cols="12" md="3" sm="12" v-if="this.currentUser.body">
+
+          <v-card max-width="500" class="mx-auto">
+            <v-img
+              max-width="500"
+              :src="require('@/assets/nutrition-plan.jpg')"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>Tes plans nutritionnels</v-card-title>
+            </v-img>
+
+            <v-card-actions>
+              <v-btn text color="#f14156" @click="$router.push({ name: 'NutritionPlan'})">
+                Tes plans nutritionnels
+              </v-btn>
+              <v-spacer></v-spacer>
+             
+            </v-card-actions>
+          </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'Home',
+
+  computed: {
+    ...mapGetters([
+      "currentUser"
+    ])
+  }
+  
+}
+</script>
+<style lang="scss">
+.panel {
+  padding: 1rem;
+}
+
+.divider {
+  padding-bottom: 5rem;
+}
+</style>
